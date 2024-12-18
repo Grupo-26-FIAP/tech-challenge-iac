5d954391899cb96d3ae9b65ad78c47ca808a6354
Jhoni Faria <jhonifarias16@gmail.com>
2024-12-17 21:58:41 -0300

    fix: rename bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tech-challenge-backend-tf"
+    bucket = "eks-tech-challenge-backend-tf"
     key = "tech-challenge/terraform.tfstate"
     region = "us-east-1"
   }
